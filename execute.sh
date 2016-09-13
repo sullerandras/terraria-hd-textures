059331c897cecc0f9132bac7894e64937fcc029c
Andras Suller <suller.andras@gmail.com>
2016-09-13 21:31:04 +0800

    Support 1.3.3; fix copying images

--- execute.sh
+++ execute.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-SOURCE_XNB_FOLDER=Terraria.v1.3.2-read-only/Content/Images
+SOURCE_XNB_FOLDER=Terraria.v1.3.3-read-only/Content/Images
 EXTRACTED_FOLDER=temp1
 DOWNSCALED_FOLDER=temp2
 NO_SEPARATORS_FOLDER=temp3
 MAGNIFIED_FOLDER=temp4
 REFILLED_FOLDER=temp5
 RELEASE_FOLDER=temp6-release
-TARGET_XNB_FOLDER=Terraria.v1.3.2/Content/Images
+TARGET_XNB_FOLDER=Terraria.v1.3.3/Content/Images
 
 function extractPngsFromTerraria() {
     echo "calling TExtract $1 => $2"
@@ -33,9 +33,11 @@ function magnifyPngs() {
     echo "magnifying images $1 => $2"
     mkdir -p $2
     mkdir -p $2/UI
-    rsync -ax --delete-after $1/*.png $2/Others/
     mkdir -p $2/Items
+    rsync -ax --delete-after $1/ $2/Others/
+    rsync -ax --delete-after $1/UI/ $2/UI/
     mv $2/Others/Item_* $2/Items/
+    rm -rf $2/Others/UI/
     if [ "$3" = "blend" ]; then
         wine tools/image_filter.exe "XBR" -wrap $2/Items $2
         wine tools/image_filter.exe "XBRz" $2/Others $2
@@ -71,7 +73,7 @@ function createRelease() {
     rm -rf $3/Images/Misc
     rm -rf $3/Images/UI/WorldGen
     rm -rf $3/Images/UI/Button*
-    echo "Enhanced version of the textures of Terraria 1.3.2" > $3/README.txt
+    echo "Enhanced version of the textures of Terraria 1.3.3" > $3/README.txt
     echo "" >> $3/README.txt
     echo "Crated by Andras Suller, `date +%F`, $version." >> $3/README.txt
     echo "For more information visit: http://forums.terraria.org/index.php?threads/enhanced-version-of-the-textures-of-terraria-1-3-0-8.39115/" >> $3/README.txt
@@ -86,5 +88,5 @@ removeSeparators $DOWNSCALED_FOLDER $NO_SEPARATORS_FOLDER
 magnifyPngs $NO_SEPARATORS_FOLDER $MAGNIFIED_FOLDER "blend"
 refillMissingPixels $EXTRACTED_FOLDER $MAGNIFIED_FOLDER $REFILLED_FOLDER
 pngsToXnbs $REFILLED_FOLDER $TARGET_XNB_FOLDER
-createRelease v0.6-1.3.2 $TARGET_XNB_FOLDER $RELEASE_FOLDER
-createRelease v0.6-noblend-1.3.2 $TARGET_XNB_FOLDER $RELEASE_FOLDER
+createRelease v0.7-1.3.3 $TARGET_XNB_FOLDER $RELEASE_FOLDER
+# createRelease v0.7-noblend-1.3.3 $TARGET_XNB_FOLDER $RELEASE_FOLDER
