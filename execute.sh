a982b80d2a7ebc25f642b262d5123f9be96b4bb4
Danny Piper <ddjpiper28@gmail.com>
2020-05-26 22:37:13 +0100

    Updated to 1.4.0.* texture pack fully

--- execute.sh
+++ execute.sh
@@ -1,40 +1,39 @@
 #!/usr/bin/env bash
 
-set -e # exit on error
+#set -e # exit on error
 set -x # echo executed lines
 
-SOURCE_XNB_FOLDER=Terraria.v1.4.0.2-read-only/Content/Images
-EXTRACTED_FOLDER=temp1
-DOWNSCALED_FOLDER=temp2
-NO_SEPARATORS_FOLDER=temp3
-MAGNIFIED_FOLDER=temp4
-REFILLED_FOLDER=temp5
-RELEASE_FOLDER=temp6-release
-TARGET_XNB_FOLDER=Terraria.v1.4.0.2/Content/Images
+SOURCE_XNB_FOLDER=~/.local/share/Steam/steamapps/common/Terraria/Content/Images
+EXTRACTED_FOLDER=EXTRACTED_FOLDER
+DOWNSCALED_FOLDER=DOWNSCALED_FOLDER
+NO_SEPARATORS_FOLDER=NO_SEPARATORS_FOLDER
+MAGNIFIED_FOLDER=MAGNIFIED_FOLDER
+REFILLED_FOLDER=REFILLED_FOLDER
+RELEASE_FOLDER=pack/Content
 
 function extractPngsFromTerraria() {
+    mkdir $2
     echo "calling TExtract $1 => $2"
-    java -jar "tools/TExtract 1.6.0.jar" --outputDirectory $2.temp $1
-    mv $2.temp/Images $2
-    rm -rf $2.temp
+    java -jar "tools/TExtract.jar" --outputDirectory $2 $1
 }
 function downscalePngs() {
     echo "downscaling images $1 => $2"
     mkdir -p $2
     mkdir -p $2/UI
-    wine tools/downscale_pngs.exe "$1" "$2"
-    wine tools/downscale_pngs.exe "$1/UI" "$2/UI"
+    wine tools/downscale_pngs.exe $1/Images $2
+    wine tools/downscale_pngs.exe $1/Images/UI $2/UI
 }
 function removeSeparators() {
     echo "removing separators $1 => $2"
     mkdir -p $2
     mkdir -p $2/UI
-    python tools/remove_separators.py "$1" "$2"
-    python tools/remove_separators.py "$1/UI" "$2/UI"
+    python tools/remove_separators.py $1 $2
+    python tools/remove_separators.py $1/UI $2/UI
 }
 function magnifyPngs() {
     echo "magnifying images $1 => $2"
-    mkdir -p $2
+    mkdir -p $2rm -rf $EXTRACTED_FOLDER
+
     mkdir -p $2/UI
     mkdir -p $2/Items
     rsync -ax --delete-after $1/ $2/Others/
@@ -55,60 +54,40 @@ function refillMissingPixels() {
     echo "refilling missing pixels in Walls and Tiles $2 => $3"
     mkdir -p $3
     mkdir -p $3/UI
-    wine tools/refill_missing_pixels.exe $1 $2 $3
-    wine tools/refill_missing_pixels.exe $1/UI $2/UI $3/UI
-}
-function pngsToXnbs() {
-    echo "converting to XNB's $1 => $2"
-    mkdir -p $2
-    mkdir -p $2/UI
-    wine tools/png_to_xnb.exe $1 $2
-    wine tools/png_to_xnb.exe $1/UI $2/UI
+    wine tools/refill_missing_pixels.exe $1/Images $2 $3
+    wine tools/refill_missing_pixels.exe $1/Images/UI $2/UI $3/UI
 }
 function createRelease() {
     version=$1
-    out_file=Images-$version.zip
-    echo "Creating zip file Images-$version.zip with all XNB's"
+    echo "Making pack folder"
     mkdir -p $3/Images
-    rm -f $out_file
-    rsync -ax --delete-after $2 $3
-    rm -rf $3/Images/Accessories
-    rm -rf $3/Images/Armor
-    rm -rf $3/Images/Backgrounds
-    rm -rf $3/Images/Misc
-    rm -rf $3/Images/SplashScreens
-    rm -rf $3/Images/TownNPCs
-    rm -rf $3/Images/UI/Bestiary
-    rm -rf $3/Images/UI/CharCreation
-    rm -rf $3/Images/UI/Creative
-    rm -rf $3/Images/UI/Minimap
-    rm -rf $3/Images/UI/PlayerResourceSets
-    rm -rf $3/Images/UI/WorldCreation
-    rm -rf $3/Images/UI/WorldGen
-    rm -rf $3/Images/UI/Button*
+    cp $2 $3
     echo "Enhanced version of the textures of Terraria 1.4.0.2" > $3/README.txt
     echo "" >> $3/README.txt
+    rm pack/pack.json
     echo " {
-    "Name": "HD Textures",
-    "Author": "sullerandras",
-    "Description": "HD Textures",
-    "Version": {
-        "major": 1,
-        "minor": 0
+    \"Name\": \"HD Textures\",
+    \"Author\": \"sullerandras\",
+    \"Description\": \"HD Textures\",
+    \"Version\": {
+        \"major\": 1,
+        \"minor\": 0
     }
-}" >> pack.json
-    echo "Crated by Andras Suller, `date +%F`, $version." >> $3/README.txt
+}" >> pack/pack.json
+    echo "Crated by Andras Suller, modified by Danny Piper for Terraria 1.4.0.* texture pack `date +%F`, $version." >> $3/README.txt
     echo "For more information visit: http://forums.terraria.org/index.php?threads/enhanced-version-of-the-textures-of-terraria-1-3-0-8.39115/" >> $3/README.txt
-    cd $3
-    zip -r ../$out_file README.txt Images
-    cd ..
 }
-#SOURCE_XNB_FOLDER="/home/andras/Downloads/Terraria_Soft_Pack_1-10-2016"
-# extractPngsFromTerraria $SOURCE_XNB_FOLDER $EXTRACTED_FOLDER
-# downscalePngs $EXTRACTED_FOLDER $DOWNSCALED_FOLDER
-# removeSeparators $DOWNSCALED_FOLDER $NO_SEPARATORS_FOLDER
-# magnifyPngs $NO_SEPARATORS_FOLDER $MAGNIFIED_FOLDER "blend"
-# refillMissingPixels $EXTRACTED_FOLDER $MAGNIFIED_FOLDER $REFILLED_FOLDER
-# pngsToXnbs $REFILLED_FOLDER $TARGET_XNB_FOLDER
-createRelease v0.10-1.4.0.2 $TARGET_XNB_FOLDER $RELEASE_FOLDER
-# createRelease v0.8-noblend-1.3.4.2 $TARGET_XNB_FOLDER $RELEASE_FOLDER
+
+ extractPngsFromTerraria $SOURCE_XNB_FOLDER $EXTRACTED_FOLDER
+ downscalePngs $EXTRACTED_FOLDER $DOWNSCALED_FOLDER
+ removeSeparators $DOWNSCALED_FOLDER $NO_SEPARATORS_FOLDER
+ magnifyPngs $NO_SEPARATORS_FOLDER $MAGNIFIED_FOLDER "blend"
+ refillMissingPixels $EXTRACTED_FOLDER $MAGNIFIED_FOLDER $REFILLED_FOLDER
+ createRelease v0.10-1.4.0.2 $REFILLED_FOLDER $RELEASE_FOLDER
+
+echo "Deleting temp folders"
+rm -rf $EXTRACTED_FOLDER
+rm -rf $DOWNSCALED_FOLDER
+rm -rf $NO_SEPARATORS_FOLDER
+rm -rf $MAGNIFIED_FOLDER
+rm -rf $REFILLED_FOLDER
